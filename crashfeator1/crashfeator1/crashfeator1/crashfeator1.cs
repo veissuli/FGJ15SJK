81c25811f054c9f284127b44a88445579e8181b3
E-A999 <Oliver.kempas@gmail.com>
2015-01-24 13:34:11 +0200

    es

--- crashfeator1/crashfeator1/crashfeator1/crashfeator1.cs
+++ crashfeator1/crashfeator1/crashfeator1/crashfeator1.cs
@@ -8,6 +8,7 @@ using Jypeli.Widgets;
 
 public class crashfeator1 : PhysicsGame
 {
+    PhysicsObject pallo1;
     public override void Begin()
     {
         
@@ -18,18 +19,34 @@ public class crashfeator1 : PhysicsGame
         // RÄJÄHDYS 
         //TAUSTA 
         //LIIKE TUNNISTUS
-        
-        
-        
-        valikko();
+
+
+
+        pelaa();
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
     void valikko()
     {
+
     }
     void pelaa()
+    {
+        pallo1 = new PhysicsObject(30, 30);
+        pallo1.Color = Color.Red;
+        Add(pallo1);
+    }
+    protected override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+    {
+        base.Update(gameTime);
+        if (pallo1 != null)
+        {
+            pallo1.Position = Mouse.PositionOnScreen;
+        }
+
+    }
+    void generoi()
     {
     }
 }
