0de31ae57a92d5f09d65159fad766a850c98e4a2
veissuli <mama@netikka.fi>
2015-01-24 14:03:48 +0200

    Lisätty valikko

--- crashfeator1/crashfeator1/crashfeator1/crashfeator1.cs
+++ crashfeator1/crashfeator1/crashfeator1/crashfeator1.cs
@@ -9,9 +9,17 @@ using Jypeli.Widgets;
 public class crashfeator1 : PhysicsGame
 {
     PhysicsObject pallo1;
+    List<Label> valikonKohdat;
+    bool pelikaynnissa;
     public override void Begin()
     {
-        
+
+        valikko();
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+
+
         // TO DO LIST!!!
         // PELAAJA HIIRI
         // SATUNNAINEN PALLOPOLKU
@@ -22,31 +30,66 @@ public class crashfeator1 : PhysicsGame
 
 
 
-        pelaa();
+
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
     void valikko()
     {
+        pelikaynnissa = false;
+        ClearAll();
+        IsMouseVisible = true;
+        valikonKohdat = new List<Label>();
 
+        Label kohta1 = new Label("Start the game");
+        kohta1.Position = new Vector(0, 40);
+        valikonKohdat.Add(kohta1);
+        Mouse.ListenOn(kohta1, MouseButton.Left, ButtonState.Pressed, Startpainettu, "Start the game");
+        foreach (Label valikonKohta in valikonKohdat)
+        {
+            Add(valikonKohta);
+        }
     }
     void pelaa()
     {
         pallo1 = new PhysicsObject(30, 30);
         pallo1.Color = Color.Red;
         Add(pallo1);
+        pallo1.Shape = Shape.Circle;
     }
     protected override void Update(Microsoft.Xna.Framework.GameTime gameTime)
     {
         base.Update(gameTime);
+        if (!pelikaynnissa)
+        {
+            foreach (Label kohta in valikonKohdat)
+            {
+                if (Mouse.IsCursorOn(kohta))
+                {
+                    kohta.TextColor = Color.Red;
+                }
+                else
+                {
+                    kohta.TextColor = Color.Black;
+                }
+            }
+        }
+
         if (pallo1 != null)
         {
             pallo1.Position = Mouse.PositionOnScreen;
         }
-
     }
     void generoi()
     {
     }
+    void Startpainettu()
+    {
+        pelikaynnissa = true;
+        ClearGameObjects();
+        pelaa();
+        IsMouseVisible = false;
+    }
+
 }
