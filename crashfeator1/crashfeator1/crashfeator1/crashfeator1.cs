791ba54e1fc7b7c8b731cd95f864f1bd8c4e6d64
E-A999 <Oliver.kempas@gmail.com>
2015-01-25 14:15:52 +0200

    Mergetty. Generaattorit pailakkaan.

--- crashfeator1/crashfeator1/crashfeator1/crashfeator1.cs
+++ crashfeator1/crashfeator1/crashfeator1/crashfeator1.cs
@@ -8,8 +8,6 @@ using Jypeli.Widgets;
 
 public class crashfeator1 : PhysicsGame
 {
-
-    
     DoubleMeter voimaMittari;
     static int TUHOUTUMINEN = 5;
     static double KAMERANOPEUS = 500.0;
@@ -24,24 +22,14 @@ public class crashfeator1 : PhysicsGame
     List<Label> valikonKohdat;
     bool pelikaynnissa;
     List<PhysicsObject> lälälä;
+    Image lentokuva = LoadImage("lentokone1");
+    Image lentokuva2 = LoadImage("lentokone2");
     public override void Begin()
     {
+#if DEBUG
         SetWindowSize(800, 600);
-        
-
-
+#endif
         valikko();
-        
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
-
-
-       
-        //LIIKE TUNNISTUS
-
-
-
-
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
@@ -52,7 +40,7 @@ public class crashfeator1 : PhysicsGame
         ClearAll();
         IsMouseVisible = true;
         valikonKohdat = new List<Label>();
-        
+
 
         Label kohta1 = new Label("Start the game");
         kohta1.Position = new Vector(0, 40);
@@ -66,14 +54,14 @@ public class crashfeator1 : PhysicsGame
     }
     void pelaa()
     {
-        
-       pallo1 = new PhysicsObject(30, 30);
-       pallo1.Color = Color.Red;
-       Add(pallo1);
-       pallo1.Shape = Shape.Circle;
-       pallo1.IgnoresExplosions = true;
 
-        generoi();
+        pallo1 = new PhysicsObject(30, 30);
+        pallo1.Color = Color.Red;
+        Add(pallo1);
+        pallo1.Shape = Shape.Circle;
+        pallo1.IgnoresExplosions = true;
+
+        generoi1();
 
         piilari = new GameObject(1, 1);
         piilari.IsVisible = false;
@@ -147,28 +135,7 @@ public class crashfeator1 : PhysicsGame
 
 
     }
-    void generoi()
-    {
-        lälälä = new List<PhysicsObject>();
-        Vector alkupiste = pallo1.Position;
-        Angle suunta1 = RandomGen.NextAngle();
-        Vector tp = alkupiste;
-
-        for (int i = 0; i < 50; i++)
-        {
-            if (i % KAANTOVALI == 0)
-            {
-                Angle känsä = RandomGen.NextAngle(
-                    Angle.FromDegrees(-KAANTOKULMA),
-                    Angle.FromDegrees(KAANTOKULMA));
-                suunta1 = känsä;
-            }
-            Vector vp = Vector.FromLengthAndAngle(70, suunta1);
-            tp = tp + vp;
-            Vector newtp = new Vector(tp.X, tp.Y);
-            Timer.SingleShot(2.0 + PALLONTEKONOPEUS * i, () => luotahti(newtp, RandomGen.NextColor()));
-        }
-    }
+    
     void Startpainettu()
     {
         alotuspainikekuuntelija.Destroy();
@@ -203,8 +170,6 @@ public class crashfeator1 : PhysicsGame
             bg.Tag = "background";
             Add(bg, -3);
             bg.Image = stars;
-
-
         }
 
         //Level.Background.Image = taustaKuva;
@@ -223,11 +188,34 @@ public class crashfeator1 : PhysicsGame
         matta.Destroying += () => voimaMittari.Value += 1;
         lälälä.Add(matta);
         AddCollisionHandler(matta, pallo1, mittaritayttyy);
-        
-       
+
+
         piilari.MoveTo(matta.Position, KAMERANOPEUS);
     }
-    void generoi2()
+    int generoi1()
+    {
+        lälälä = new List<PhysicsObject>();
+        Vector alkupiste = pallo1.Position;
+        Angle suunta1 = RandomGen.NextAngle();
+        Vector tp = alkupiste;
+
+        for (int i = 0; i < 50; i++)
+        {
+            if (i % KAANTOVALI == 0)
+            {
+                Angle känsä = RandomGen.NextAngle(
+                    Angle.FromDegrees(-KAANTOKULMA),
+                    Angle.FromDegrees(KAANTOKULMA));
+                suunta1 = känsä;
+            }
+            Vector vp = Vector.FromLengthAndAngle(70, suunta1);
+            tp = tp + vp;
+            Vector newtp = new Vector(tp.X, tp.Y);
+            Timer.SingleShot(2.0 + PALLONTEKONOPEUS * i, () => luotahti(newtp, RandomGen.NextColor()));
+        }
+        return 50;
+    }
+    int generoi2()
     {
         lälälä = new List<PhysicsObject>();
         Vector alkupiste = pallo1.Position;
@@ -248,84 +236,173 @@ public class crashfeator1 : PhysicsGame
             Vector newtp = new Vector(tp.X, tp.Y);
             Timer.SingleShot(1.6 + PALLONTEKONOPEUS * i, () => luotahti(newtp, RandomGen.NextColor()));
         }
+        return 50;
     }
-    void generoi3()
+    int generoi3()
     {
+        lälälä = new List<PhysicsObject>();
+        Vector alkupiste = pallo1.Position;
+        Angle suunta1 = RandomGen.NextAngle();
+        Vector tp = alkupiste;
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % KAANTOVALI < 1)
+            {
+                Angle känsä = RandomGen.NextAngle(
+                    Angle.FromDegrees(-KAANTOKULMA - 20),
+                    Angle.FromDegrees(KAANTOKULMA + 20));
+                suunta1 = känsä;
+            }
+            Vector vp = Vector.FromLengthAndAngle(94, suunta1);
+            tp = tp + vp;
+            Vector newtp = new Vector(tp.X, tp.Y);
+            Timer.SingleShot(1.7 + PALLONTEKONOPEUS * i, () => luotahti(newtp, RandomGen.NextColor()));
+
+        }
+        return 100;
     }
-    void generoi4()
+    int generoi4()
     {
+        lälälä = new List<PhysicsObject>();
+        Vector alkupiste = pallo1.Position;
+        Angle suunta1 = RandomGen.NextAngle();
+        Vector tp = alkupiste;
+
+        for (int i = 0; i < 60; i++)
+        {
+            if (i % KAANTOVALI < 6)
+            {
+                Angle känsä = RandomGen.NextAngle(
+                    Angle.FromDegrees(-KAANTOKULMA - 20),
+                    Angle.FromDegrees(KAANTOKULMA + 20));
+                suunta1 = känsä;
+            }
+            Vector vp = Vector.FromLengthAndAngle(94, suunta1);
+            tp = tp + vp;
+            Vector newtp = new Vector(tp.X, tp.Y);
+            Timer.SingleShot(1.7 + PALLONTEKONOPEUS * i * 2, () => luotahti(newtp, Color.Aqua));
+        }
+        return 60;
     }
-    void generoi5()
+    int generoi5()
     {
+        lälälä = new List<PhysicsObject>();
+        Vector alkupiste = pallo1.Position;
+        Angle suunta1 = RandomGen.NextAngle();
+        Vector tp = alkupiste;
+
+        for (int i = 0; i < 80; i++)
+        {
+            if (i % KAANTOVALI < 6)
+            {
+                Angle känsä = RandomGen.NextAngle(
+                    Angle.FromDegrees(-KAANTOKULMA - 45),
+                    Angle.FromDegrees(KAANTOKULMA + 45));
+                suunta1 = känsä;
+            }
+            Vector vp = Vector.FromLengthAndAngle(94, suunta1);
+            tp = tp + vp;
+            Vector newtp = new Vector(tp.X, tp.Y);
+            Timer.SingleShot(1.7 + PALLONTEKONOPEUS * i * 4, () => luotahti(newtp, Color.Red));
+        }
+        return 80;
     }
     void lv()
     {
+        Surfaces aweryq = Level.CreateBorders();
+        for (int i = 0; i < 30; i++)
+        {
+            luolentsikka(new Vector(RandomGen.NextInt(-300, 300), Screen.Bottom));
+
+        }
+        Timer.SingleShot(32, delegate { puhdista(aweryq); });
     }
 
-
-
-
-
-   void voimamittari()
+    void luolentsikka(Vector paikka)
     {
-     voimaMittari = new DoubleMeter(0);
-     voimaMittari.MaxValue = 50;
-     ProgressBar voimaPalkki = new ProgressBar(150, 10);
-     voimaPalkki.X = Screen.Left + 150;
-     voimaPalkki.Y = Screen.Top - 70;
-     voimaMittari.Value = 0;
-     voimaPalkki.BorderColor = Color.Aqua;
-     voimaPalkki.BindTo(voimaMittari);
-     voimaPalkki.Image = LoadImage("palkki tyhj");
-     voimaPalkki.BarImage = LoadImage("palkki sini");
-       
-       Add(voimaPalkki);
-     
-       
-     voimaMittari.UpperLimit += VoimaMittariTaynna;
+        PhysicsObject lentsikka = new PhysicsObject(50, 50);
+        lentsikka.Position = paikka;
+        Add(lentsikka);
+
+        lentsikka.Image = lentokuva;
+        Vector ljk = new Vector(Screen.Center.X, Screen.Top);
+
+
+        RandomMoverBrain satunnaisAivot = new RandomMoverBrain(200);
+        satunnaisAivot.ChangeMovementSeconds = 3;
+        lentsikka.Brain = satunnaisAivot;
+
+        lentsikka.LifetimeLeft = TimeSpan.FromSeconds(30);
+    }
+    void puhdista(Surfaces areusure)
+    {
+        areusure.Left.Destroy();
+        areusure.Right.Destroy();
+        areusure.Top.Destroy();
+        areusure.Bottom.Destroy();
     }
 
+    void voimamittari()
+    {
+        voimaMittari = new DoubleMeter(0);
+        voimaMittari.MaxValue = 50;
+        ProgressBar voimaPalkki = new ProgressBar(150, 10);
+        voimaPalkki.X = Screen.Left + 150;
+        voimaPalkki.Y = Screen.Top - 70;
+        voimaMittari.Value = 0;
+        voimaPalkki.BorderColor = Color.Aqua;
+        voimaPalkki.BindTo(voimaMittari);
+        voimaPalkki.Image = LoadImage("palkki tyhj");
+        voimaPalkki.BarImage = LoadImage("palkki sini");
 
+        Add(voimaPalkki);
+
+
+        voimaMittari.UpperLimit += VoimaMittariTaynna;
+    }
 
     void VoimaMittariTaynna()
-   {
-     
-       int luku = RandomGen.NextInt(1, 6);
-       if (luku == 1)
-       {
-           generoi();
-       }
-      
-       if (luku == 2)
-       {
-           generoi2();
-       }
-      
-       if (luku == 3)
-       {
-           generoi3();
-       }
-       if (luku == 4)
-       {
-           generoi4();
-       }
-       if (luku == 5)
-       {
-           generoi5();
-       }
-       if (luku == 6)
-       {
-           lv();
-       }
-    }
+    {
 
+        int luku = RandomGen.NextInt(1, 6);
+        int pallojatulossa = 0;
+        if (luku == 1)
+        {
+            pallojatulossa=generoi1();
+        }
+
+        if (luku == 2)
+        {
+            pallojatulossa = generoi2();
+        }
+
+        if (luku == 3)
+        {
+            pallojatulossa = generoi3();
+        }
+        if (luku == 4)
+        {
+            pallojatulossa = generoi4();
+        }
+        if (luku == 5)
+        {
+            pallojatulossa = generoi5();
+        }
+        if (luku == 6)
+        {
+            lv();
+        }
+        voimaMittari.Value = 0.0;
+        voimaMittari.MaxValue = pallojatulossa;
+    }
 
     void mittaritayttyy(PhysicsObject matta, PhysicsObject pallo1)
     {
-       Explosion rajahdys = new Explosion(100);
-       rajahdys.Position = matta.Position;
-       matta.Destroy();
-       Add(rajahdys);
+        Explosion rajahdys = new Explosion(100);
+        rajahdys.Position = matta.Position;
+        matta.Destroy();
+        Add(rajahdys);
 
     }
     void teksti()
